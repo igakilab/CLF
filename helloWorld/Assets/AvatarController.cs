b323a084446c85964339c705b4a062cccc71fc3f
KanazawaT <e1q19024@oit.ac.jp>
2021-10-07 16:27:18 +0900

    コミットし忘れ分

--- helloWorld/Assets/AvatarController.cs
+++ helloWorld/Assets/AvatarController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 // MonoBehaviourPunCallbacksを継承して、photonViewプロパティを使えるようにする
 public class AvatarController : MonoBehaviourPunCallbacks
 {
+    Camera camera;
+
+    private void Start()
+	{
+        camera = Camera.main;
+	}
+
     private void Update()
     {
+        
         // 自身が生成したオブジェクトだけに移動処理を行う
         if (photonView.IsMine)
         {
             var input = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
             transform.Translate(6f * Time.deltaTime * input.normalized);
+
+            Vector3 playerPos = this.transform.position;
+            camera.transform.position = new Vector3(playerPos.x, playerPos.y, -10);
         }
     }
 }
