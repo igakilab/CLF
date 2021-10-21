2b17f7c28a1d99ea8b542a3e07550e5d2823bbdc
KanazawaT <e1q19024@oit.ac.jp>
2021-10-21 16:39:29 +0900

    プレイヤー同士の衝突削除したつもり あと移動方法を物理演算に変更

--- helloWorld/Assets/AvatarController.cs
+++ helloWorld/Assets/AvatarController.cs
@@ -22,25 +22,25 @@ public class AvatarController : MonoBehaviourPunCallbacks
             var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
             //transform.Translate(6f * Time.deltaTime * input.normalized);
             //this.rb.AddForce(input);
-            this.rb.velocity = input*5.0f;
-
+            //this.rb.velocity = input*5.0f;
+            this.rb.AddForce(input);
             Vector3 playerPos = this.transform.position;
             camera.transform.position = new Vector3(playerPos.x, playerPos.y, -10);
         }
     }
 
-    private string playerTag = "Player";
+    //private string playerTag = "Player";
     private string chestTag = "Chest";
     private int chestCount = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Debug.Log("âΩÇ©Ç∆ê⁄êGÇµÇΩ");
-        if (collision.collider.tag == playerTag)
+        /*if (collision.collider.tag == playerTag)
         {
             SceneManager.LoadScene("GameOverScene");
             Debug.Log("ìGÇ∆ê⁄êGÇµÇΩÅI");
         
-        }
+        }*/
         if(collision.collider.tag == chestTag)
         {
             Debug.Log("ïÛî†Ç∆ê⁄êGÇµÇΩÅI");
