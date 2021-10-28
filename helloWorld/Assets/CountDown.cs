877891572eda25c60f399e6e768662220b9c83da
KanazawaT <e1q19024@oit.ac.jp>
2021-10-28 14:21:17 +0900

    タイマーの開始をゲーム開始時に

--- helloWorld/Assets/CountDown.cs
+++ helloWorld/Assets/CountDown.cs
@@ -8,15 +8,16 @@ public class CountDown : MonoBehaviour
     private float time = 60.0f;
     public Text timerText;
     public bool isTimeUp;
-
+ 	public GameObject photonController;
     void Start()
     {
         isTimeUp = false;
+        photonController = GameObject.Find("photonControler");
     }
 
     void Update()
     {
-        if (0 < time)
+        if (0 < time  && photonController.GetComponent<SampleScene>().isGaming())
         {
             time -= Time.deltaTime;
             timerText.text = time.ToString("F1");
